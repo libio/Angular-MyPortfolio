7aca6a29e3cfedd390d6f4268738f01ec4724fcc
libio Armando Quispe Hancco <134541@unsaac.edu.pe>
2022-01-26 17:37:34 -0500

    Subiendo componeste desarrollados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
