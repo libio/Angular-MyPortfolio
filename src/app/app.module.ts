63af724fea0802caa7d8a9ad82fe83a989d97321
libio Armando Quispe Hancco <134541@unsaac.edu.pe>
2022-01-26 22:09:03 -0500

    subiendo components desarrollados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { FactsComponent } from './components/facts/facts.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { ServicesComponent } from './components/services/services.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BackComponent } from './components/back/back.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    AboutComponent,
+    FactsComponent,
+    SkillsComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    FooterComponent,
+    BackComponent
   ],
   imports: [
     BrowserModule,
